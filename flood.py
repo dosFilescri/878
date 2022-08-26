83c1cd485dc33f84e5014cca43d33d3d5f2d45e6
dosFilescri <112200360+dosFilescri@users.noreply.github.com>
2022-08-26 22:23:20 +0300

    Update flood.py

--- flood.py
+++ flood.py
@@ -28,7 +28,7 @@ def run():
 			addr = (str(ip),int(port))
 			for x in range(times):
 				s.sendto(data,addr)
-			print(i +" Sent!!!")
+			print(i +" Sent!!!               "+data)
 		except:
 			print("[!] Error!!!")
 
